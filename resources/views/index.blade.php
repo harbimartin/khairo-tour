40af9017f6421471404c70d0b7e42c00f6f325e1
priodwisembodo23 <priodwisembodo23@gmail.com>
2021-11-04 16:29:20 +0700

    push login

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css">
+    <title>E-Budgeting PT. KBS</title>
+</head>
+<body>
+    <h1>Hallo</h1>
+    @yield('content')
+</body>
+<script src="https://rawgit.com/moment/moment/2.2.1/min/moment.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    @yield('script')
+</html>
---
+++ resources/views/navigation.blade.php
@@ -0,0 +1,10 @@
+@extends('index')
+
+@section('content')
+    <div>
+        <div class="avatar inline-flex md:block md:text-center">
+            <img src="{{url('/assets/avatars.svg')}}" alt="avatar">
+            <h6>Username Users</h6>
+        </div>
+    </div>
+@endsection
